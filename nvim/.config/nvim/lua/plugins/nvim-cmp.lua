ee268a85d0c9315d7c3661ed9f3bbff14fd5a7f4
fazaimron27 <cincaumyself@gmail.com>
2025-04-10 20:25:46 +0700

    enable rounded border nvim-cmp `LazyVim`

---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    window = {
+      completion = {
+        border = "rounded",
+        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+      },
+    },
+  },
+}
